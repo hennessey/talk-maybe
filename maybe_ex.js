ed0ec0ffe71b4c38a3696ddf8f8fb91911059740
john.hennessey <john.hennessey@ef.com>
2018-05-17 13:26:40 -0400

    removed confusing chain example

--- maybe_ex.js
+++ maybe_ex.js
@@ -9,20 +9,5 @@ const yellToTheRooftops = (str) => yell(makeLoud(str));
 
 const safeYellToTheRooftops = (str) => Maybe.of(str)
     .map(yell)
-    .map(makeLoud);
-//const fancy_safeYellToTheRooftops = compose(map(yell), map(makeLoud), Maybe.of);
-
-
-const safeString = (fn) => (str) => Maybe.of(fn(str));
-
-//safeYell :: string -> Maybe string
-const safeYell = safeString((str) => `${str}`);
-
-//safeMakeLoud :: string -> Maybe string
-const safeMakeLoud = safeString((str) => str.toUpperCase());
-
-const debug = (val) => { console.log(val); return val; };
-
-const extra_safeYellToTheRooftops = compose(map(safeYell), map(safeMakeLoud), debug);
-
-const extra_fancy_safeYellToTHeRooftops = compose(chain(safeYell), debug, chain(safeMakeLoud), debug);
+    .map(makeLoud); 
+ //const fancy_safeYellToTheRooftops = compose(map(yell), map(makeLoud), Maybe.of);
